cfe5e94216fea74ecf1a31a0f459f1d5bdafccf2
prashantjoshiiamprashu-star <prashantjoshi.iamprashu@gmail.com>
2026-01-05 10:23:52 +0530

    Update index.tsx

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -39,14 +39,14 @@ const Header = () => {
         <div className="flex w-full items-center justify-between xl:w-1/4">
           <a href="/">
             <Image
-              src="/images/logo/bitmorph-logo-dark.svg"
+              src="/images/logo/b-dark.jpg"
               alt="Bitmorph Solutions"
               width={160}
               height={40}
               className="hidden w-full dark:block"
             />
             <Image
-              src="/images/logo/bitmorph-logo-light.svg"
+              src="/images/logo/b-light.jpg"
               alt="Bitmorph Solutions"
               width={160}
               height={40}
